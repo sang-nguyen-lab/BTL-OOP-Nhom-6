ed12a0f3e5d4131a6028393ed0302417923049ce
sang-nguyen-lab <sang48192@gmail.com>
2025-10-14 22:55:46 +0700

    F4.h

--- F4.h
+++ F4.h
@@ -8,18 +8,9 @@ int ma_ve,ngay,thang,nam;
 int so_ghe;
 string loai_ve;//loại vip,thường ,bình dân
 public:
-ve_may_bay(){
-  ma_ve=0;
-  ngay=0;
-  thang=0;
-  nam=0;
-  so_ghe=0;
-  loai_ve="null";
-}
-~ve_may_bay(){
-  cout<<"đã hủy ve may bay mã: "<<ma_ve<<endl;
-}
-
-}
+ve_may_bay();
+~ve_may_bay();
 void nhap_ve();
 void in_thong_tin_ve();
+int cap_nhat_soghe();
+}
