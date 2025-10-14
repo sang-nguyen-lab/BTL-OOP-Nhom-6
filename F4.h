23551d41a066649db357e515d55d09b0a336d8ee
sang-nguyen-lab <sang48192@gmail.com>
2025-10-14 22:44:57 +0700

    F4.h

--- F4.h
+++ F4.h
@@ -4,4 +4,19 @@
 #include <string>
 using namespace std;
 class ve_may_bay{
-
+int ma_ve,ngay,thang,nam;
+int so_ghe;
+string loai_ve;//loại vip,thường ,bình dân
+public:
+ve_may_bay(){
+  ma_ve=0;
+  ngay=0;
+  thang=0;
+  nam=0;
+  so_ghe=0;
+  loai_ve="null";
+}
+~ve_may_bay(){
+  cout<<"đã hủy ve may bay mã: "<<ma_ve<<endl;
+}
+}
